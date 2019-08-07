aa7a5f9966ac85a001e61f2d97b98dfd9d67e73a
dzucconi <mail@damonzucconi.com>
2019-08-07 12:16:04 -0400

    feat(typography): adds `line-height` and `font-size` functions; adds `type-treatment` mixin

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const colors = require("./data/colors");
 const themes = require("./data/themes");
+const typography = require("./data/typography");
 
 module.exports = {
   includePaths: [path.join(__dirname, "lib/assets/stylesheets")],
   data: {
     colors,
-    themes
+    themes,
+    typography
   }
 };
