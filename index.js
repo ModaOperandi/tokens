7722a0ef2e2a9964d3bef6b22d972137ab34b83b
dzucconi <mail@damonzucconi.com>
2019-08-20 17:46:23 -0400

    Adds space: scale + z-indexes

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require("path");
 const colors = require("./data/colors");
 const themes = require("./data/themes");
 const typography = require("./data/typography");
+const space = require("./data/space");
 
 module.exports = {
   includePaths: [path.join(__dirname, "lib/assets/stylesheets")],
   data: {
     colors,
     themes,
-    typography
+    typography,
+    space
   }
 };
