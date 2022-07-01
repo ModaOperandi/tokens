3bfb2d90327faec2bb8a861b473cde16626b9d0e
Brian Miller <brian.miller@modaoperandi.com>
2022-07-01 00:04:57 -0400

    style: update yarn to 1.22.19; add linting; clean up a lot of things

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,36 +1,39 @@
 import * as colorString from 'color-string';
-import { colors } from "./colors";
-import { space } from "./space";
-import { typography } from "./typography";
+import { colors } from './colors';
+import { space } from './space';
+import { typography } from './typography';
 
 export type Font = keyof typeof typography.fonts;
-export type TextTreatment = keyof typeof typography["text-treatments"];
+export type TextTreatment = keyof typeof typography['text-treatments'];
 export type Color = keyof typeof colors.all;
 
-export const text = (name: TextTreatment, font = "sans" as Font) => {
-  const treatment = typography["text-treatments"][name];
+export const text = (name: TextTreatment, font = 'sans' as Font) => {
+  const treatment = typography['text-treatments'][name];
   return {
     ...treatment,
-    "font-family": typography.fonts[font].join(","),
-    "line-height": `${treatment["line-height"]}`,
+    'font-family': typography.fonts[font].join(','),
+    'line-height': `${treatment['line-height']}`
   };
 };
 
 export const remToUnitlessPx = (value: string) =>
-  parseFloat(value) * parseInt(typography["root-font-size"], 0);
+  parseFloat(value) * parseInt(typography['root-font-size'], 10);
 
 export const color = (name: Color, alpha?: number) => {
   const color = colors.all[name];
-  return alpha != null ? colorString.to.rgb([...(colorString.get.rgb(color)?.slice(0, 3) || []), alpha]) : color;
-}
+  return alpha != null
+    ? // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+      colorString.to.rgb([...(colorString.get.rgb(color)?.slice(0, 3) || []), alpha])
+    : color;
+};
 
 export const spacing = (
-  ty: number | string,
-  rx?: number | string,
-  b?: number | string,
-  l?: number | string
+  topY: number | string,
+  rightX?: number | string,
+  bottom?: number | string,
+  left?: number | string
 ) =>
-  [ty, rx, b, l].reduce((acc, v) => {
-    if (v === undefined) return acc;
-    return [acc, space.scale[v] ?? v].join(" ");
-  }, "");
+  [topY, rightX, bottom, left].reduce((acc, value) => {
+    if (value === undefined) return acc;
+    return [acc, space.scale[value] ?? value].join(' ');
+  }, '');
